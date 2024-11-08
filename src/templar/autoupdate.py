166c675bedbefa7941adae76fb3ee10f7a01bfd5
distributedstatemachine! <fatheroffire@proton.me>
2024-11-08 17:03:28 +0000

    debug

--- src/templar/autoupdate.py
+++ src/templar/autoupdate.py
@@ -181,7 +181,7 @@ class AutoUpdate(threading.Thread):
         if "PM2_HOME" in os.environ:
             # PM2 will restart the process if we exit
             logger.info("Detected PM2 environment. Exiting for PM2 to restart the process.")
-            sys.exit(0)
+            sys.exit(1)
         elif os.getenv("RUNNING_IN_DOCKER") == "true" or os.path.exists('/.dockerenv'):
             # In Docker, it's better to exit and let the orchestrator handle restarts
             logger.info("Detected Docker environment. Exiting for Docker to restart the container.")

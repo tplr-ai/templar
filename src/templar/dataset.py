38c121d87073bb40dbaefb9c3a515a17da5de97a
distributedstatemachine! <fatheroffire@proton.me>
2024-12-29 07:58:44 +0000

    fix: dataset , scoring

--- src/templar/dataset.py
+++ src/templar/dataset.py
@@ -184,7 +184,7 @@ class SubsetLoader(IterableDataset):
 
 
 class DatasetLoader(SubsetLoader):
-    name: str = "HuggingFaceFW/fineweb-edu-score-2"
+    name: str = "airtrain-ai/fineweb-edu-fortified"
     rows_base_url: str = "https://datasets-server.huggingface.co/rows"
     size_base_url: str = "https://datasets-server.huggingface.co/size"
 

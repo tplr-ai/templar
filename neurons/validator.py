ec8c7c1d681d00bc01320de8b96581bd8d450cce
distributedstatemachine <fatheroffire@proton.me>
2025-03-01 22:32:03 +0000

    feat: bin mov avg 0

--- neurons/validator.py
+++ neurons/validator.py
@@ -204,7 +204,7 @@ class Validator:
             self.metagraph.n, dtype=torch.float32
         )
         self.binary_moving_averages = torch.full(
-            (self.metagraph.n,), 0.5, dtype=torch.float32
+            (self.metagraph.n,), 0.0, dtype=torch.float32
         )
         self.weights = torch.zeros(self.metagraph.n, dtype=torch.float32)
         self.normalised_binary_moving_averages = torch.zeros(

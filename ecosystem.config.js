1b15f672da7d44bd95549b65bdaef778f9b5456e
Joel Lidin <joellidin@gmail.com>
2025-04-10 22:59:49 +0200

    Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,43 +1,32 @@
 require('dotenv').config(); // Loads environment variables from .env
 
-const NUM_MINERS = process.env.NUM_MINERS || 5;
-const PROJECT_NAME = `templar-test`;
+const NUM_MINERS = process.env.NUM_MINERS || 3;
+const PROJECT_NAME = `scoring-base`;
 const LOCAL_FLAG = process.env.LOCAL === 'true' ? ' --local' : '';
 
-// Create array of miner configurations
-// const minerConfigs = [...Array(Number(NUM_MINERS))].map((_, index) => ({
-//     name: `TM${index}`,
-//     script: "neurons/miner.py",
-//     interpreter: "python3",
-//     env: {
-//         ...process.env,
-//         PROJECT_NAME: PROJECT_NAME
-//     },
-//     args: `--wallet.name miner --wallet.hotkey M${index} --device cpu --subtensor.network local --netuid 2 --use_wandb --project "${PROJECT_NAME}"${LOCAL_FLAG}`
-// }));
+const minerConfigs = [...Array(Number(NUM_MINERS))].map((_, index) => ({
+    name: `TM${index}`,
+    script: "neurons/miner.py",
+    interpreter: "python3",
+    env: {
+        ...process.env,
+        PROJECT_NAME: PROJECT_NAME
+    },
+        args: `--wallet.name miner${index + 1} --wallet.hotkey default --device cuda:${index} --subtensor.network local --netuid 2 --use_wandb --project "${PROJECT_NAME}"`
+}));
 
 module.exports = {
     apps: [
-        // ...minerConfigs,
-        // {
-        //     name: "TV1",
-        //     script: "neurons/validator.py",
-        //     interpreter: "python3",
-        //     env: {
-        //         ...process.env,
-        //         PROJECT_NAME: PROJECT_NAME
-        //     },
-        //     args: `--wallet.name validator --wallet.hotkey default --device cpu --subtensor.network local --netuid 2 --use_wandb --project "${PROJECT_NAME}"${LOCAL_FLAG}`
-        // },
+        ...minerConfigs,
         {
-            name: "Aggregator",
-            script: "neurons/aggregator.py",
+            name: "TV1",
+            script: "neurons/validator.py",
             interpreter: "python3",
             env: {
                 ...process.env,
                 PROJECT_NAME: PROJECT_NAME
             },
-            args: "--netuid 3 --device cuda --project templar"
+            args: `--wallet.name validator --wallet.hotkey default --device cpu --subtensor.network local --netuid 2 --use_wandb --project "${PROJECT_NAME}"`
         }
     ]
 }
